ff0dd9fe3ab99748343bd2baa40a144640f6dddc
Zhengming Cao <zhengming.cao@tomtom.com>
2019-10-24 16:00:30 +0200

    make some changes

---
+++ main.tf
@@ -0,0 +1,45 @@
+module "scaffold" {
+#  source    = "../terraform-module-scaffold"
+  source    = "github.com/gitpls/terraform-module-scaffold"
+}
+resource "azurerm_resource_group" "webapps" {
+    name        = "webapps"
+    location    = "${var.loc}"
+    tags        = "${var.tags}"
+}
+
+resource "random_string" "webapprnd" {
+  length  = 8
+  lower   = true
+  number  = true
+  upper   = false
+  special = false
+}
+
+resource "azurerm_app_service_plan" "free" {
+    count               = "${length(var.webapplocs)}"
+    name                = "plan-free-${var.webapplocs[count.index]}"
+    location            = "${var.webapplocs[count.index]}"
+    resource_group_name = "${azurerm_resource_group.webapps.name}"
+    tags                = "${azurerm_resource_group.webapps.tags}"
+    reserved            = true
+    kind                = "Linux"
+    sku {
+        tier = "Free"
+        size = "F1"
+    }
+}
+
+resource "azurerm_app_service" "citadel" {
+    count               = "${length(var.webapplocs)}"
+    name                = "webapp-${random_string.webapprnd.result}-${var.webapplocs[count.index]}"
+    location            = "${var.webapplocs[count.index]}"
+    resource_group_name = "${azurerm_resource_group.webapps.name}"
+    tags                = "${azurerm_resource_group.webapps.tags}"
+
+    app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
+}
+
+output "webappids" {
+  value = "${azurerm_app_service.citadel.*.id}"
+}
